7a7ac2ed68e20be3175af08b83e4d9ff77a41d32
koumen222 <koumenprive@gmail.com>
2026-03-12 19:31:17 +0100

    s

--- Backend/models/Workspace.js
+++ Backend/models/Workspace.js
@@ -77,7 +77,7 @@ const workspaceSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.Mixed,
     default: {}
   },
-  // Unique token for Ssssshopify webhook URL per workspaces
+  // Unique tsoken for Ssssshopify webhook URL per workspaces
   shopifyWebhookToken: {
     type: String,
     unique: true,
